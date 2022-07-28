fe8c725df49430f61ce0aca5c0abb2b9a6eb81eb
Gizzel-Ruiz <gruiz015@jaguar.tamu.edu>
2022-07-28 09:59:35 -0500

    bypassing login/registration functions for demo

--- phapp.js
+++ phapp.js
@@ -3,13 +3,14 @@ function regButtonClick(){
     const rlabel = document.getElementById("regErrorLabel")
 
     rbutton.addEventListener('click', function onClick(event) { 
-        // Need to add logic to check for blank fields
-        number = 0;
-        if (number == 0) {
-            rlabel.style.display="block";
-        } else{
-            rlabel.style.display="none";
-        }
+        window.location.href='feed.html'
+        // // Need to add logic to capture information from fields 
+        // number = 0;
+        // if (number == 0) {
+        //     rlabel.style.display="block";
+        // } else{
+        //     rlabel.style.display="none";
+        // }
     });
 }
 
@@ -18,12 +19,13 @@ function signinButtonClick(){
     const slabel = document.getElementById("signinErrorLabel")
 
     sbutton.addEventListener('click', function onClick(event) { 
-        // Need to add logic to validate login credentials
-        number = 0;
-        if (number == 0) {
-            slabel.style.display="block";
-        } else{
-            slabel.style.display="none";
-        }
+        window.location.href='feed.html'
+        // // Need to add logic to validate login credentials
+        // number = 0;
+        // if (number == 0) {
+        //     slabel.style.display="block";
+        // } else{
+        //     slabel.style.display="none";
+        // }
     });
 }
